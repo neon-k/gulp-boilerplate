deda4f897676ab119970e641aba9b4d11bb56835
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-06-04 20:16:08 +0900

    テスト用フォルダ削除

--- src/about/script/index.ts
+++
@@ -1,9 +0,0 @@
-import gsap from 'gsap';
-
-(() => {
-  window.addEventListener('DOMContentLoaded', () => {
-    console.log('dddddd');
-
-    console.log(gsap);
-  });
-})();
--- src/about/test/script/index.ts
+++
@@ -1,9 +0,0 @@
-import gsap from 'gsap';
-
-(() => {
-  window.addEventListener('DOMContentLoaded', () => {
-    console.log('ssssssss');
-
-    console.log(gsap);
-  });
-})();
